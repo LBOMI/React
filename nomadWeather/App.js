0b8a8359e6e4bf6156714c53daa9c0383bd75301
LBOMI <125543534+LBOMI@users.noreply.github.com>
2024-11-22 14:31:43 +0900

    Create App.js

---
+++ nomadWeather/App.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor:"pink"
+  },
+  text: {
+    fontSize: 28,
+    color: "blue"
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500"
+  },
+  weather: {
+    flex: 3,
+    
+  },
+  day: {
+    flex: 1,
+    alignItems:"center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: "165",
+  },
+  description: {
+    fontSize: "40"
+  }
+});
