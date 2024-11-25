5d0734915be45fd4b8676fe083a52a03a25546c4
LBOMI <125543534+LBOMI@users.noreply.github.com>
2024-11-25 22:13:58 +0900

    Create App.js

---
+++ WorkHardTravelHardApp/App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TextInput } from 'react-native';
+import { theme } from './colors';
+import { useState } from 'react';
+
+export default function App() {
+  const [working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+        <Text style={{...styles.btnText, color: working ? "white":theme.grey}}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+        <Text style={{...styles.btnText, color: !working ? "white":theme.grey}}>Travel</Text>
+        </TouchableOpacity>
+      
+      </View>
+      <View>
+          <TextInput style = {styles.input}></TextInput>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+  },
+  input : {
+    backgroundColor: "white",
+  }
+});
