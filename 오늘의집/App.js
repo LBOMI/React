2b8f454b2596c877594947d21f0feac492f8a7a8
LBOMI <125543534+LBOMI@users.noreply.github.com>
2024-12-22 23:12:39 +0900

    Update App.js

--- 오늘의집/App.js
+++ 오늘의집/App.js
@@ -18,45 +18,71 @@ function Header() {
       <div className='sticky-container' style={{height: 80.75}}>
         <div className='sticky-container_1'>
          <div className='sticky-container_1-1'>
+          <div className='sticky-container_1-1_1'>
+            <button type='button'>
+              <span></span>
+            </button>
+          </div>
           <div className='logo'>
-            <div className='logo_O'>
-            <img src="오늘의 집 logo.jpg" width="71" height="28" fill="none"></img>
-            </div>
+            <a>
+            <img src="오늘의 집 logo.jpg" width="71" height="28"></img>
+            </a>
           </div>
 
           <div className='logo_N'>
-            <div className='logo_N_1' style={{width: 71, height:54, color:"#35C5F0"}}>커뮤니티</div>
-            <div className='logo_N_1' style={{width: 40.52, height:54}}>쇼핑</div>
-            <div className='logo_N_1' style={{width: 107.94, height:54}}>인테리어/생활</div>
+            <a>
+              <span style={{color:"#35C5F0"}}>커뮤니티</span>
+            </a>
+            <a>
+              <span>쇼핑</span>
+            </a>
+            <a>
+              <span>인테리어/생활</span>
+            </a>
           </div>
 
          
          <div className='sticky-container_2'>
-          <div className='sticky-container_input'>
-            <div className='sticky-container_input_1'>
-              <span className='sticky-container_input_icon'><FontAwesomeIcon icon={faMagnifyingGlass} /></span>
-              <input type='text' placeholder='통합검색' autoComplete='off' aria-autocomplete='list' ></input>
-              <div className='sticky-container_input_2'></div>
-            </div> 
+          <div className='sticky-container_2_1'>
+            <div className='sticky-container_2_1_1'>
+              <div className='sticky-container_2_1_1_1'>
+                <div className='sticky-container_2_1_1_1_1'>
+                  <span><FontAwesomeIcon icon={faMagnifyingGlass} /></span>
+                  <input 
+                    type='text' 
+                    placeholder='통합검색' 
+                    autoComplete='off' 
+                    aria-autocomplete='list'>
+                  </input>
+                  <div className='sticky-container_2_1_1_1_1_1'></div>
+                </div>
+              </div>
             </div>
-          <div className='s-c_shoppingCart'>
-            <span className='s-c_shoppingCart_icon'><FontAwesomeIcon icon={faCartShopping}/></span>
+            <div className='sticky-container_2_1_2'>
+              <button type='button'>
+                <span></span>
+              </button>
+            </div>
+            <a className='sticky-container_2_1_3a'>
+              <span><FontAwesomeIcon icon={faCartShopping}/></span>
+            </a>
+            <div className='sticky-container_2_1_4'>
+              <div className='sticky-container_2_1_4_1'>
+                <a>로그인</a>
+                <a>회원가입</a>
+                <a>고객센터</a>
+              </div>
+              <span>
+                <button role='button' type='button'>
+                  <span className='sticky-container_2_1_4_2'>글쓰기</span>
+                  <span className='sticky-container_2_1_4_3'><FontAwesomeIcon icon={faChevronDown} /></span>
+                </button>
+              </span>
+            </div>
           </div>
-
-          <div className='s-c_3'>
-            <div className='s-c_3_1'>
-              <a className='s-c_3_1_login'>로그인</a>
-              <a className='s-c_3_1_signUp'>회원가입</a>
-              <a className='s-c_3_1_signUp'>고객센터</a>
-            </div>
-            <span style={{lineHeight: 0}}>
-              <button role='button' type='button' className='s-c_3_button'>
-                <span className='s-c_3_button_p'>글쓰기</span>
-                <span className='s-c_3_button_icon'><FontAwesomeIcon icon={faChevronDown} /></span>
-                </button>   
-            </span>
-            </div>
+          
           </div>
+          
          </div>
       </div>
       </div>
@@ -65,146 +91,146 @@ function Header() {
           <div className='s-c_4'>
             <div  className='s-c_4_1'>
               <nav className='s-c_4_nav'>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_3_p'>홈</p>
+                <span>
+                  <div className='s-c_4_1_1'>
+                    <a>
+                      <div className='s-c_4_1_1_2'>
+                        <p>홈</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
+                <span>
                   <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>추천</p>
+                    <a>
+                      <div className='s-c_4_1_2_1'>
+                        <p>추천</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>#채널</p>
+                <span>
+                  <div className='s-c_4_1_3'>
+                    <a>
+                      <div className='s-c_4_1_3_1'>
+                        <p>#채널</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>집들이</p>
+                <span>
+                  <div className='s-c_4_1_4'>
+                    <a>
+                      <div className='s-c_4_1_4_1'>
+                        <p>집들이</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>집사진</p>
+                <span>
+                  <div className='s-c_4_1_5'>
+                    <a>
+                      <div className='s-c_4_1_5_1'>
+                        <p>집사진</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>3D인테리어</p>
+                <span>
+                  <div className='s-c_4_1_6'>
+                    <a>
+                      <div className='s-c_4_1_6_1'>
+                        <p>3D인테리어</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>살림수납</p>
+                <span>
+                  <div className='s-c_4_1_7'>
+                    <a>
+                      <div className='s-c_4_1_7_1'>
+                        <p>살림수납</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>반려동물</p>
+                <span>
+                  <div className='s-c_4_1_8'>
+                    <a>
+                      <div className='s-c_4_1_8_1'>
+                        <p>반려동물</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>육아</p>
+                <span>
+                  <div className='s-c_4_1_9'>
+                    <a>
+                      <div className='s-c_4_1_9_1'>
+                        <p>육아</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>홈스토랑</p>
+                <span>
+                  <div className='s-c_4_1_10'>
+                    <a>
+                      <div className='s-c_4_1_10_1'>
+                        <p>홈스토랑</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>플랜테리어</p>
+                <span>
+                  <div className='s-c_4_1_11'>
+                    <a>
+                      <div className='s-c_4_1_11_1'>
+                        <p>플랜테리어</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>콜렉터블</p>
+                <span>
+                  <div className='s-c_4_1_12'>
+                    <a>
+                      <div className='s-c_4_1_12_1'>
+                        <p>콜렉터블</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>캠핑</p>
+                <span>
+                  <div className='s-c_4_1_13'>
+                    <a>
+                      <div className='s-c_4_1_13_1'>
+                        <p>캠핑</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>취미</p>
+                <span>
+                  <div className='s-c_4_1_14'>
+                    <a>
+                      <div className='s-c_4_1_14_1'>
+                        <p>취미</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>핫플레이스</p>
+                <span>
+                  <div className='s-c_4_1_15'>
+                    <a>
+                      <div className='s-c_4_1_15_1'>
+                        <p>핫플레이스</p>
                       </div>
                     </a>
                   </div>
                 </span>
-                <span className='s-c_4_1_1'>
-                  <div className='s-c_4_1_2'>
-                    <a className='s-c_4_1_a'>
-                      <div className='s-c_4_1_3'>
-                        <p className='s-c_4_1_4_p'>이벤트</p>
+                <span>
+                  <div className='s-c_4_1_16'>
+                    <a>
+                      <div className='s-c_4_1_16_1'>
+                        <p>이벤트</p>
                       </div>
                     </a>
                   </div>
@@ -1861,11 +1887,14 @@ function Dropdown() {
       <div className='drop_1'>
         <div className='drop_1_1'>
           <span>
-            <a>캠핑</a>
+            <a>취미</a>
           </span>
-          <span></span>
-          <span></span>
-          <span></span>
+          <span>
+            <a>핫플레이스</a>
+          </span>
+          <span>
+            <a>이벤트</a>
+          </span>
         </div>
       </div>
     </div>
